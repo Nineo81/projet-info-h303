a9396fd0c9dfea67377cc1d23a4a59311a1acd40
bcoquele <boris.coquelet@ulb.ac.be>
2019-04-22 11:32:37 +0200

    work on gui in progress

---
+++ src/view/ConnectionController.java
@@ -0,0 +1,31 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConnectionController {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button register;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
+

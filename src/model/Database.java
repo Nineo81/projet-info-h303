5a6408b853d5eb4e66441c2574ae40101e30d42c
bcoquele <boris.coquelet@ulb.ac.be>
2019-05-04 11:05:13 +0200

    Adding database class for connection to the database

---
+++ src/model/Database.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private String CONNECTION_URL = "jdbc:derby:" + "Database" + ";create=false";
+    private Connection conn = null;
+    private static Database instance = null;
+
+    /***
+     * Private constructor to prevent multiple instantiation
+     */
+    private Database(){
+        try{
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+            this.conn = DriverManager.getConnection(CONNECTION_URL);
+        }
+        catch(Throwable e)  {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Open a connection to Derby
+     */
+    public void open(){
+        try {
+            conn = DriverManager.getConnection(CONNECTION_URL);
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+            //TO DO : gérer l'exception
+        }
+    }
+
+    /***
+     * Return the only instance of the DAO (it's a singleton).
+     * The synchronized is mandatory because of the multi-threads coming from JavaFX.
+     * @return instance of DAO
+     */
+    public synchronized static Database getInstance(){
+        if ( instance == null){
+            instance = new Database();
+        }
+        return instance;
+    }
+}
